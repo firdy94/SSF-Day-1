12d1171ce90158d89006ad67c223506d1258ba2f
Firdaus <mohdfirdaus94@outlook.com>
2022-01-06 10:33:47 +0800

    Add port configuration

--- src/main/java/ibf/ssf/day1workshop/Day1workshopApplication.java
+++ src/main/java/ibf/ssf/day1workshop/Day1workshopApplication.java
@@ -1,6 +1,7 @@
 package ibf.ssf.day1workshop;
 
 import java.util.Collections;
+import java.util.Properties;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.DefaultApplicationArguments;
@@ -11,32 +12,28 @@ import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class Day1workshopApplication {
-	private static final Logger logger = LoggerFactory.getLogger(Day1workshopApplication.class);
 	private static final String DEFAULT_PORT = "3000";
 
 	public static void main(String[] args) {
-		logger.info("Workshop 11");
 		SpringApplication myApp = new SpringApplication(Day1workshopApplication.class);
 		// SpringApplication.run(Day1workshopApplication.class, args);
 		String port = "null";
 		ApplicationArguments cliOptions = new DefaultApplicationArguments(args);
 		if (cliOptions.containsOption("port")) {
 			port = cliOptions.getOptionValues("port").get(0);
-			logger.info("port number input >>" + port);
-			if (port == null) {
-				port = System.getenv("PORT");
+			if ((port == null || port.isEmpty())) {
+				port= System.getenv("PORT")
 				if (port == null) {
 					port = DEFAULT_PORT;
-					myApp.setDefaultProperties(Collections.singletonMap("server.port", port));
-
 				}
-			} else {
-				myApp.setDefaultProperties(Collections.singletonMap("server.port", port));
 			}
+			myApp.setDefaultProperties(Collections.singletonMap("server.port", port));
 		}
+
 		// myApp.setDefaultProperties(Collections.singletonMap("server.port", port));
 		System.out.printf("Application started on port %s\n", port);
 		myApp.run(args);
+
 	}
 
 	@Bean
